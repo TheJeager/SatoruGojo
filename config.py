d38ceedde55f6a973e3918565cede2f19cee765b
Eren Yeager <theerenyeagerx@gmail.com>
2025-12-28 10:34:36 +0530

    chore: added mongodb!

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = ("API_ID",)
-API_HASH = ("API_HASH")
-BOT_TOKEN = ("BOT_TOKEN")
-
-# This is optional if you want a fallback RTMP URL
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 DEFAULT_RTMP_URL = ("rtmps://dc5-1.rtmp.t.me/s/")
+LOGGER_ID = int(os.getenv("LOGGER_ID", 0))
+MONGO_URL = os.getenv("MONGO_URL", "")
